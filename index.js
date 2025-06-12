1b5b647f1a525a2f28078ee62fa94cbb85a39f06
Your Name <minajmiha02@gmail.com>
2025-06-12 16:07:15 +0600

    added .env file

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
-
 
 // middleware
 // ZBQVyOTQIv4r7HkH
@@ -11,28 +10,24 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
 // const uri =`mongodb+srv://<db_username>:<db_password>@cluster0.63qrdth.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-const uri =`mongodb+srv://job-portal:ZBQVyOTQIv4r7HkH@cluster0.63qrdth.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-console.log(uri)
+const uri = `mongodb+srv://job-portal:ZBQVyOTQIv4r7HkH@cluster0.63qrdth.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// console.log(process.env.DB_USER);
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    
-    // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
-    // Send a ping to confirm a successful connection
-    // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -40,14 +35,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
 app.get("/", (req, res) => {
   res.send("job making server is running");
 });
 
-
 app.listen(port, () => {
   console.log(`job server are making in running on port: ${port}`);
 });
-
