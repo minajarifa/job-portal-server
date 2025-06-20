a683cded6d18095c292c014ef4a150bbf005da9d
Your Name <minajmiha02@gmail.com>
2025-06-20 20:00:36 +0600

    users get operation

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ app.use(express.json());
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.63qrdth.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
